6fd63416e318046930d9922a076a45acf0b248b0
Andrew Wen <2060andrew@gmail.com>
2021-02-23 19:19:13 -0600

    need to use correct work dirs for launching java

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,5 @@
-screen -dmS UIMA-REST java -jar /app/MedTaggerREST/UIMA-REST-SERVER.jar -Dserver.port=8080
+cd /app/MedTaggerREST/
+screen -dmS UIMA-REST java -jar UIMA-REST-SERVER.jar -Dserver.port=8080
+cd /app/N3CNLP
 screen -dmS N3CNLP mvn spring-boot:run
 /bin/bash
