fdb2b83669face4d2a427e84dfdd32be03289b3b
Andrew Wen <2060andrew@gmail.com>
2021-02-23 20:04:35 -0600

    Now that everything is working, go back to non-interactive mode

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,4 @@
 cd /app/MedTaggerREST/
 screen -dmS UIMA-REST java -jar UIMA-REST-SERVER.jar -Dserver.port=8080
-cd /app/N3CNLP
-screen -dmS N3CNLP mvn spring-boot:run
-/bin/bash
+cd /app/N3CMedTagger
+mvn spring-boot:run
